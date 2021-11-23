c0c5f03c8e976ad65f82deaf0411087950031074
jos3ma5ter <josedrn5@gmail.com>
2021-11-23 18:29:14 -0500

    primera version del frontend

---
+++ js/paralax.js
@@ -0,0 +1,59 @@
+/*================== LCARROS EN LA PLAYA=========================*/ 
+
+const imagen1 = document.querySelector(".imagen1");
+window.addEventListener("scroll", () => {
+    imagen1.style.backgroundPositionY = `${scrollY / 1.4}px`
+})
+const imagen2 = document.querySelector(".imagen2");
+window.addEventListener("scroll", () => {
+    imagen2.style.backgroundPositionY = `${scrollY / 1.5}px`
+})
+const imagen3 = document.querySelector(".imagen3");
+window.addEventListener("scroll", () => {
+    imagen3.style.backgroundPositionY = `${scrollY / 1.6}px`
+})
+const imagen4 = document.querySelector(".imagen4");
+window.addEventListener("scroll", () => {
+    imagen4.style.backgroundPositionY = `${scrollY / 2.1}px`
+})
+const imagen5 = document.querySelector(".imagen5");
+window.addEventListener("scroll", () => {
+    imagen5.style.backgroundPositionY = `${scrollY / 2.3}px`
+})
+const imagen6 = document.querySelector(".imagen6");
+window.addEventListener("scroll", () => {
+    imagen6.style.backgroundPositionY = `${scrollY / 2.5}px`
+})
+const imagen7 = document.querySelector(".imagen7");
+window.addEventListener("scroll", () => {
+    imagen7.style.backgroundPositionY = `${scrollY / 3}px`
+})
+
+
+
+/*================== LAMPARAS DE GARAJES=========================*/ 
+
+const lamp1 = document.querySelector(".lamp1");
+window.addEventListener("scroll", () => {
+    lamp1.style.backgroundPositionY = `${scrollY / 2}px`
+})
+const lamp2 = document.querySelector(".lamp2");
+window.addEventListener("scroll", () => {
+    lamp2.style.backgroundPositionY = `${scrollY / 2.1}px`
+})
+const lamp3 = document.querySelector(".lamp3");
+window.addEventListener("scroll", () => {
+    lamp3.style.backgroundPositionY = `${scrollY / 2.2}px`
+})
+const lamp4 = document.querySelector(".lamp4");
+window.addEventListener("scroll", () => {
+    lamp4.style.backgroundPositionY = `${scrollY / 2.3}px`
+})
+const lamp5 = document.querySelector(".lamp5");
+window.addEventListener("scroll", () => {
+    lamp5.style.backgroundPositionY = `${scrollY / 2.4}px`
+})
+const lamp6 = document.querySelector(".lamp6");
+window.addEventListener("scroll", () => {
+    lamp6.style.backgroundPositionY = `${scrollY / 2.5}px`
+})
